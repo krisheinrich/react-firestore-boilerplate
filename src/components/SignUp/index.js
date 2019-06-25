9f95452186cc74ce90aa947c5b8da0b0862eb508
krisheinrich <krisheinrich@gmail.com>
2019-06-24 20:01:28 -0700

    Add form to SignUp component, implement logic to save user to Firebase

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,9 +1,114 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
-const SignUp = () => (
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+
+const SignUpPage = () => (
   <div>
-    <h1>Welcome to the SignUp page!</h1>
+    <h1>SignUp</h1>
+    <SignUpForm />
   </div>
 );
 
-export default SignUp;
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  password: '',
+  passwordConfirm: '',
+  error: null
+};
+
+class _SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const { username, email, password } = this.state;
+
+    this.props.firebase
+      .createUserWithEmailAndPassword(email, password)
+      .then(authUser => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const {
+      username, email, password, passwordConfirm, error
+    } = this.state;
+
+    const isInvalid =
+      password !== passwordConfirm ||
+      password === '' ||
+      email === '' ||
+      username === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="username"
+          value={username}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="password"
+          value={password}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          name="passwordConfirm"
+          value={passwordConfirm}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign Up
+        </button>
+
+        { error && <p>{error.message}</p> }
+      </form>
+    );
+  }
+}
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+
+const SignUpForm = compose(
+  withRouter,
+  withFirebase
+)(_SignUpForm);
+
+export default SignUpPage;
+
+export { SignUpForm, SignUpLink };

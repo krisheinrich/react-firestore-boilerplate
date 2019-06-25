06313f4ccee82995a76e5e0943f50614f4b202b3
krisheinrich <krisheinrich@gmail.com>
2019-06-25 00:07:18 -0700

    Expose admin page in nav; add list of app users to admin page

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -26,11 +26,9 @@ const NavigationAuth = () => (
     <li>
       <Link to={ROUTES.ACCOUNT}>Account</Link>
     </li>
-    {/*}
     <li>
       <Link to={ROUTES.ADMIN}>Admin</Link>
     </li>
-    */ }
     <li>
       <SignOutButton />
     </li>

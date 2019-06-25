f4097936e5e1139364ca41369f343f1b92b1ec3d
krisheinrich <krisheinrich@gmail.com>
2019-06-25 11:39:26 -0700

    Add method to merge db user entity with auth user

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -35,6 +35,31 @@ class Firebase {
   updatePassword = newPassword =>
     this.auth.currentUser.updatePassword(newPassword);
 
+  // Merge auth and DB user API
+  onAuthUserListener = (next, fallback) =>
+    this.auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        this.user(authUser.uid)
+          .once('value')
+          .then(snapshot => {
+            const dbUser = snapshot.val();
+
+            if (!dbUser.roles) {
+              dbUser.roles = {};
+            }
+
+            authUser = {
+              uid: authUser.uid,
+              email: authUser.email,
+              ...dbUser,
+            };
+            next(authUser);
+          });
+      } else {
+        fallback();
+      }
+    });
+
   // User API
 
   user = uid => this.db.ref(`users/${uid}`);

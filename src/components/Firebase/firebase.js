11c06ef54c93364cee8b314906f25fce1643757e
Kris Heinrich <krisheinrich@gmail.com>
2020-04-16 11:46:59 -0700

    Add warning if mising env vars

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,6 +2,23 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
+// Modify this list as needed
+const EXPECTED_VARS = [
+  'REACT_APP_API_KEY',
+  'REACT_APP_AUTH_DOMAIN',
+  'REACT_APP_DATABASE_URL',
+  'REACT_APP_PROJECT_ID',
+  'REACT_APP_STORAGE_BUCKET',
+  'REACT_APP_MESSAGING_SENDER_ID',
+  'REACT_APP_APP_ID'
+];
+
+for (const v of EXPECTED_VARS) {
+  if (process.env[v] === undefined) {
+    console.warn("Missing the expected env var " + v);
+  }
+}
+
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
